bb40a0ee501d47af9e0a62c749c42ce16e339370
yuukla <josh.taimanglo@gmail.com>
2020-02-13 01:27:26 -0700

    adjusted, checked and runs in atom/g++ compiler

--- chankins_1.cpp
+++ chankins_1.cpp
@@ -1,24 +1,25 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <iomanip.h>
+#include <iomanip>
 #include <cmath>
 #include <math.h>
+#include <iostream>
 
 using namespace std;
 
-#DEFINE CM_PER_INCH 2.54
-#DEFINE LB_PER_KG 2.20462262
+#define CM_PER_INCH 2.54
+#define LB_PER_KG 2.20462262
 
 //calc lucky function
 double calc_lucky (int date, int month, int year, double weight, double height){
   int x, y, z, lucky;
   double foo;
-  x = 100 * (pow(month, 2));
-  y = 10 * (pow(date, 3));
-  z = ((x + y)/year);
+  x = 100 * (pow(month, 2)); //1600
+  y = 10 * (pow(date, 3)); //327680
+  z = ((x + y)/year); //23.800
   foo = (pow(weight, 6)/height);
-  lucky = static_cast<int>(z + (sqrt(foo)) mod 10) + 1;
+  lucky = (static_cast<int>(z + (sqrt(foo))) % 10) + 1;
   return lucky;
 };
 
@@ -38,14 +39,13 @@ int get_int (string prompt){
   cout << prompt;
   cout << "Enter a numeric value! ";
   cin >> n;
-  cout << endl << n <<;
   return n;
 };
 
 //get_string
 string get_string (string prompt){
   string s;
-  cout << "Prompt is " << prompt << endl;
+  cout << "Prompt is " << prompt;
   cin >> s;
   return s;
 };
@@ -57,13 +57,14 @@ int main (void){
 
   cout << "Zelda's Lucky Number Calculator" << endl;
   name = get_string("What is your name?");
-  month = get_int("In what month were your born?");
+  month = get_int("In what month were your born? ");
   date = get_int ("What was the date? ");
   year = get_int ("What was the year (yyyy)? ");
-  height_in = get_int("What is your weight in lbs?");
+  weight_lb = get_int("What is your weight in lbs?");
+  height_in = get_int("What is your height in inches? ");
   height_cm = convert_inch_to_cm(height_in);
   weight_kg = convert_lb_to_kg(weight_lb);
-  lucky = calc_lucky(date, hieght_cm, month, weight_kg, year);
+  lucky = calc_lucky(date, height_cm, month, weight_kg, year);
   cout << name << ", your lucky number is " << lucky << ". Thank you, that will be $25.";
   return 0;
 }
